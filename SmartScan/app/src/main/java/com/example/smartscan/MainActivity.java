b657c5c4dde1cd95349e532fe513b9063114cc7f
Vương Minh Hiếu <hieulklk128@gmail.com>
2025-03-04 17:08:19 +0700

    update 3

--- SmartScan/app/src/main/java/com/example/smartscan/MainActivity.java
+++ SmartScan/app/src/main/java/com/example/smartscan/MainActivity.java
@@ -1,19 +1,98 @@
 package com.example.smartscan;
 
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CardView chupAnh, nhanDienVanBan, dichVanBan, nhanDienVatThe, timKiemThongTinAnh;
+    Animation animation1, animation2, animation3, animation4, animation5, animation6, animation7, animation8, animation9, animation10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        chupAnh = findViewById(R.id.chup_anh_button);
+        nhanDienVanBan = findViewById(R.id.nhan_dien_van_ban_button);
+        dichVanBan = findViewById(R.id.dich_van_ban_button);
+        nhanDienVatThe = findViewById(R.id.nhan_dien_vat_the_button);
+        timKiemThongTinAnh = findViewById(R.id.tim_kiem_thong_tin_anh_button);
+        animation1 = AnimationUtils.loadAnimation(this, R.anim.animation1);
+        animation2 = AnimationUtils.loadAnimation(this, R.anim.animation2);
+        animation3 = AnimationUtils.loadAnimation(this,R.anim.animation3);
+        animation4 = AnimationUtils.loadAnimation(this,R.anim.animation4);
+        animation5 = AnimationUtils.loadAnimation(this,R.anim.animation5);
+        animation6 = AnimationUtils.loadAnimation(this,R.anim.animation6);
+        animation7 = AnimationUtils.loadAnimation(this,R.anim.animation7);
+        animation8 = AnimationUtils.loadAnimation(this,R.anim.animation8);
+        animation9 = AnimationUtils.loadAnimation(this,R.anim.animation9);
+        animation10 = AnimationUtils.loadAnimation(this,R.anim.animation10);
+        chupAnh.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == motionEvent.ACTION_UP) {
+                    chupAnh.startAnimation(animation1);
+                } else if(motionEvent.getAction() == motionEvent.ACTION_DOWN){
+                    chupAnh.startAnimation(animation2);
+                }
+                return true;
+            }
+        });
+        nhanDienVanBan.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == motionEvent.ACTION_UP) {
+                    nhanDienVanBan.startAnimation(animation3);
+                } else if(motionEvent.getAction() == motionEvent.ACTION_DOWN){
+                    nhanDienVanBan.startAnimation(animation4);
+                }
+                return true;
+            }
+        });
+        dichVanBan.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == motionEvent.ACTION_UP) {
+                    dichVanBan.startAnimation(animation5);
+                } else if(motionEvent.getAction() == motionEvent.ACTION_DOWN){
+                    dichVanBan.startAnimation(animation6);
+                }
+                return true;
+            }
+        });
+        nhanDienVatThe.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == motionEvent.ACTION_UP) {
+                    nhanDienVatThe.startAnimation(animation7);
+                } else if(motionEvent.getAction() == motionEvent.ACTION_DOWN){
+                    nhanDienVatThe.startAnimation(animation8);
+                }
+                return true;
+            }
+        });
+        timKiemThongTinAnh.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == motionEvent.ACTION_UP) {
+                    timKiemThongTinAnh.startAnimation(animation9);
+                } else if(motionEvent.getAction() == motionEvent.ACTION_DOWN){
+                    timKiemThongTinAnh.startAnimation(animation10);
+                }
+                return true;
+            }
+        });
     }
 }
