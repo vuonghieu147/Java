b574328f0b29c61f92a7f54a7d70fadb335a60e8
Vương Minh Hiếu <hieulklk128@gmail.com>
2025-03-04 14:46:01 +0700

    update 2

--- SmartScan/app/build.gradle.kts
+++ SmartScan/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.smartscan"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.smartscan"
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
