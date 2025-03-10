0aacb1c011b2ee68d7013a6091daaec73af5cdba
Vương Minh Hiếu <hieulklk128@gmail.com>
2025-03-10 13:06:10 +0700

    update 6

---
+++ SmartScan/app/src/main/java/com/example/smartscan/CameraX.java
@@ -0,0 +1,126 @@
+package com.example.smartscan;
+
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.Camera;
+import androidx.camera.core.ImageCaptureException;
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+
+public class CameraX extends AppCompatActivity {
+    private Button chup;
+    private ImageButton imageButton;
+    private ProcessCameraProvider cameraProvider;
+    private ImageCapture imageCapture;
+    private PreviewView previewView;
+    private Camera camera;
+    public static final int CAPTURE_MODE_MAXIMIZE_QUALITY = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camera_x);
+        imageButton = findViewById(R.id.flash);
+        chup = findViewById(R.id.chup_cameraX);
+        previewView = findViewById(R.id.preview_view);
+        imageButton.setTag(R.drawable.baseline_flash_on_24);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 101);
+        }
+        moCamera();
+        chup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chupLuuAnh();
+            }
+        });
+    }
+
+    private void moCamera(){
+        ListenableFuture<ProcessCameraProvider> listenableFuture =
+                ProcessCameraProvider.getInstance(this);
+
+        listenableFuture.addListener(() -> {
+            try {
+                cameraProvider = listenableFuture.get();
+
+                Preview preview = new Preview.Builder().build();
+                imageCapture = new ImageCapture.Builder().build();
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
+
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+                cameraProvider.unbindAll();
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+                imageButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if((int)imageButton.getTag() == R.drawable.baseline_flash_off_24){
+                            camera.getCameraControl().enableTorch(true);
+                            imageButton.setBackgroundResource(R.drawable.baseline_flash_on_24);
+                            imageButton.setTag(R.drawable.baseline_flash_on_24);
+                        } else if ((int)imageButton.getTag() == R.drawable.baseline_flash_on_24) {
+                            camera.getCameraControl().enableTorch(false);
+                            imageButton.setBackgroundResource(R.drawable.baseline_flash_off_24);
+                            imageButton.setTag(R.drawable.baseline_flash_off_24);
+                        }
+                    }
+                });
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+    private void chupLuuAnh(){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "smartscan_" + System.currentTimeMillis() + ".jpg");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/SmartScan");
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(getContentResolver(), MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).build();
+        imageCapture.takePicture(outputFileOptions,ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback(){
+
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                Uri anhUri = outputFileResults.getSavedUri();
+                Intent intent = new Intent();
+                intent.putExtra("anhUri",anhUri.toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                Toast.makeText(CameraX.this,"Chụp ảnh thất bại",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- SmartScan/app/src/main/java/com/example/smartscan/TextRecognition.java
+++ SmartScan/app/src/main/java/com/example/smartscan/TextRecognition.java
@@ -1,20 +1,49 @@
 package com.example.smartscan;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.Manifest;
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class TextRecognition extends AppCompatActivity {
-
-    private Button back;
+    private Button back, chupAnh, chonAnh, saoChepVanBan;
+    private TextView vanBan;
+    private ImageView hinhAnh;
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //startActivity(new Intent(TextRecognition.this,CameraX.class));
+                layAnh.launch(new Intent(TextRecognition.this,CameraX.class));
+            } else {
+                Toast.makeText(this, "Bạn cần cấp quyền để sử dụng camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private final ActivityResultLauncher<Intent> layAnh = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result ->{
+        if(result.getResultCode() == RESULT_OK && result.getData() != null){
+            Intent duLieu = result.getData();
+            String anhString = duLieu.getStringExtra("anhUri");
+            Uri uri = Uri.parse(anhString);
+            hinhAnh.setImageURI(uri);
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +52,12 @@ public class TextRecognition extends AppCompatActivity {
 
         Log.d("ActivityCheck", "1111");
         back = findViewById(R.id.back_button);
+        chupAnh = findViewById(R.id.chup_anh);
+        chonAnh = findViewById(R.id.chon_anh);
+        saoChepVanBan = findViewById(R.id.copy_text);
+        hinhAnh = findViewById(R.id.hinh_anh);
+        vanBan = findViewById(R.id.text_result);
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +65,24 @@ public class TextRecognition extends AppCompatActivity {
                 finish();
             }
         });
+        chupAnh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Context context = view.getContext();
+                if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission, 1);
+                    }
+                    else{
+                        //startActivity(new Intent(TextRecognition.this,CameraX.class));
+                        layAnh.launch(new Intent(TextRecognition.this,CameraX.class));
+                    }
+                }else {
+                    Toast.makeText(context, "Thiết bị của bạn không có chức năng chụp ảnh", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 }
